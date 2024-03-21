4008a3f5292b89ca2cfa1bbd47a5eac2b8f0a33b
i3elj <guisande.bernardo@gmail.com>
2024-03-21 16:58:16 -0300

    scaffould of deleting and editing html

--- public/js/context-menu.js
+++ public/js/context-menu.js
@@ -4,32 +4,50 @@ const menu_wrapper = htmx.find('#__ctx-menu-wrapper')
 const menu = htmx.find('#__ctx-menu')
 
 menu_wrapper.onmousedown = function(event) {
-    if (event.target == this)
-        deactivateMenu()
+	if (event.target == this)
+		deactivateMenu()
 }
 
 export function contextmenu(event)
 {
-    event.preventDefault()
+	event.preventDefault()
 
-    var x = event.pageX
-    var y = event.pageY
+	var x = event.pageX
+	var y = event.pageY
 
-    menu.style.top = `${y + 8}px`
-    menu.style.left = `${x + 32}px`
+	menu.style.top = `${y + 8}px`
+	menu.style.left = `${x + 32}px`
 
-    activateMenu()
+	activateMenu(event)
 }
 
-function activateMenu()
+function activateMenu(event)
 {
-    menu_wrapper.style.opacity = 1
-    menu_wrapper.style.zIndex = 2
-    const ctx_menu_tag = htmx.find('#__ctx-menu_tag')
-    ctx_menu_tag.innerText = html.get_tag_name(event.target)
+	menu_wrapper.style.opacity = 1
+	menu_wrapper.style.zIndex = 2
+	const ctx_menu_tag = htmx.find('#__ctx-menu_tag')
+	ctx_menu_tag.innerText = html.get_tag_name(event.target)
+
+	const [
+		delete_btn,
+		html_btn,
+		styles_btn
+	] = Array.from(menu.children).filter(e => e.tagName.toLowerCase() == 'button')
+
+	delete_btn.onclick = () => {
+		const id = html.get_id(event.target)
+		htmx.find('.' + id).remove()
+		deactivateMenu()
+	}
+	html_btn.onclick = () => {
+		const id = html.get_id(event.target)
+		const element = htmx.find('.' + id)
+		console.log(element.outerHTML)
+	}
+	styles_btn.onclick = function(event) {}
 }
 
 function deactivateMenu() {
-    menu_wrapper.style.opacity = 0
-    menu_wrapper.style.zIndex = -1
+	menu_wrapper.style.opacity = 0
+	menu_wrapper.style.zIndex = -1
 }
--- public/js/html_manipulation.js
+++ public/js/html_manipulation.js
@@ -1,58 +1,55 @@
 export function create_unique_id()
 {
-    const id_list = Array.from(htmx.findAll('.__item'))
-                         .map(e => get_id(e).split('-')[1])
+	const id_list = Array.from(htmx.findAll('.__item'))
+						 .map(e => get_id(e).split('-')[1])
 
-    console.log(id_list)
+	if (id_list.length == 0)
+		return '__item-1'
 
-    if (id_list.length == 0)
-        return '__item-1'
-
-    return '__item-' + (Math.max(...id_list) + 1)
+	return '__item-' + (Math.max(...id_list) + 1)
 }
 
 export function get_id(elem) { return elem.classList[0] }
 
 export function add_id(elem, id) {
-    elem.classList = [id].concat([...elem.classList]).join(' ')
+	elem.classList = [id].concat([...elem.classList]).join(' ')
 }
 
 export function get_tag_name(elem) {
-    return elem.tagName.toLowerCase()
+	return elem.tagName.toLowerCase()
 }
 
 export function clone(element)
 {
-    const tag_name = get_id(element).split('__')[0]
-    const clone = document.createElement(tag_name)
-    clone.classList.add('__item')
-    clone.classList.add('__droppable')
-    clone.setAttribute('draggable', true)
-    add_id(clone, create_unique_id())
+	const tag_name = get_id(element).split('__')[0]
+	const clone = document.createElement(tag_name)
+	clone.classList.add('__item')
+	clone.classList.add('__droppable')
+	clone.setAttribute('draggable', true)
+	add_id(clone, create_unique_id())
 
-    switch (get_tag_name(clone)) {
-        case 'h1':
-        case 'p':
-            console.log('hi?')
-            clone.innerHTML = 'Insert Text'
-            clone.onblur = e => clone.setAttribute('contenteditable', false)
-            clone.ondblclick = e => {
-                clone.setAttribute('contenteditable', true)
-                clone.focus()
-                window.getSelection().selectAllChildren(clone)
-            }
-            setTimeout(() => {
-                clone.setAttribute('contenteditable', true)
-                clone.focus()
-                window.getSelection().selectAllChildren(clone)
-            }, 100)
-            break;
+	switch (get_tag_name(clone)) {
+		case 'h1':
+		case 'p':
+			clone.innerHTML = 'Insert Text'
+			clone.onblur = e => clone.setAttribute('contenteditable', false)
+			clone.ondblclick = e => {
+				clone.setAttribute('contenteditable', true)
+				clone.focus()
+				window.getSelection().selectAllChildren(clone)
+			}
+			setTimeout(() => {
+				clone.setAttribute('contenteditable', true)
+				clone.focus()
+				window.getSelection().selectAllChildren(clone)
+			}, 100)
+			break;
 
-        case 'textarea':
-            clone.style.resize = 'both'
-            clone.removeAttribute('disabled')
-            break;
-    }
+		case 'textarea':
+			clone.style.resize = 'both'
+			clone.removeAttribute('disabled')
+			break;
+	}
 
-    return clone
+	return clone
 }
